dd6ae17de9320ddbbbc04ddee50b46da4c556152
hemanth260399 <hemanthraja26@gmail.com>
2025-01-24 22:47:48 +0530

    s

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(session({
     saveUninitialized: true,
     cookie: {
         secure: "auto",
-        sameSite: "lax"
+        sameSite: "strict",
     }
 }))
 app.use(passport.initialize());
