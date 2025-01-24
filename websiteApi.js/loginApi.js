08eb1879120e07d98c1a8c44c3701cc4bd274893
hemanth260399 <hemanthraja26@gmail.com>
2025-01-24 08:31:06 +0530

    testing

--- websiteApi.js/loginApi.js
+++ websiteApi.js/loginApi.js
@@ -98,6 +98,10 @@ loginServer.get("/auth/success", (req, res) => {
 })
 loginServer.get("/auth/login/failure", (req, res) => res.status(401).json({ msg: "Failed to authenticate with Google" }))
 loginServer.get("/auth/loginSuccess", (req, res) => {
-    let token = jwtToken({ data: req.user.userdata.email }, "1h")
-    res.json({ msg: "Login successful", data: { name: req.user.userdata.name, email: req.user.userdata.email, id: req.user.userdata._id }, token: token })
+    try {
+        let token = jwtToken({ data: req.user.userdata.email }, "1h")
+        res.json({ msg: "Login successful", data: { name: req.user.userdata.name, email: req.user.userdata.email, id: req.user.userdata._id }, token: token })
+    } catch (e) {
+        res.status(500).json({ msg: "Something went wrong" })
+    }
 })
